5a047fd27a56157cd797c76aa8f32eba3137ce91
haider1196 <haider.ahmed1196@gmail.com>
2017-09-23 15:21:53 -0400

    Fixed the navbar

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('run');
+$(document).ready(function() {
+      $(window).on('scroll', function() {
+        if (Math.round($(window).scrollTop()) > 100) {
+          $('.navbar').addClass('scrolled');
+        } else {
+          $('.navbar').removeClass('scrolled');
+        }
+      });
+    });
+    console.log('run');
