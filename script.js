7af843e1ac0e918c8eb1beec66369d48767be9e1
Mitul Shah <mitulxshah@gmail.com>
2017-09-24 04:35:25 -0400

    fixed searching issue in serach by creating a new search.js file

--- script.js
+++ script.js
@@ -4,30 +4,6 @@ function flash(){
     document.querySelector('.update').style.display = "inherit";
 }
 
-let filterInput = document.getElementById('search');
-filterInput.addEventListener('keyup', filterNames);
-
-function filterNames(){
-  //get the value
-  let filterValue = document.getElementById('search').value.toUpperCase();
-
-  // get names ul
-  let ul = document.getElementById('collection');
-  //get li's from ul
-  let li = ul.querySelectorAll('li.item');
-
-  // loop
-  for(let i = 0; i < li.length; i++){
-    let a = li[i].getElementsByTagName('h4')[0];
-    //if it matches correct
-    if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
-      li[i].style.display = '';
-    } else {
-      li[i].style.display = 'none';
-    }
-  }
-}
-
 document.getElementById("filter").addEventListener("mousemove", function(){
     document.getElementById("filter").getElementsByClassName("highlight")[0].style.fontWeight = "bold";
     document.getElementById("filter").getElementsByClassName("highlight")[0].className = "";
